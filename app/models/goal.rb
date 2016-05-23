7177141d43baece81663fcbefcffafb7ba12f256
Isaac Tsai <isaac@adup.com.tw>
2016-05-23 17:57:06 +0800

    Bug fixed: 轉換目標Hash物件無法parse為JSON，直接存為JSON

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,7 +3,7 @@ class Goal < ActiveRecord::Base
 
   def self.create_by_user_than_fetch_and_save user
     ana = Analytics.new user
-    json = ana.list_goals.to_h
+    json = ana.list_goals.to_json
     self.create(user: user, json: json)
   end
 end
