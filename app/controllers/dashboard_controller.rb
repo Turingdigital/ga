9a1b2346547fd9cea452cf29243e070083722d9e
Isaac Tsai <isaac@adup.com.tw>
2016-05-27 15:35:38 +0800

    Dashboard太龐大，先重構拆任務到小項目

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,17 @@ class DashboardController < ApplicationController
 
   before_action :authorize, only: [:index]
 
+  def create_event_sessions
+    @event_sessions = @analytics.get_event_sessions(profile_id)
+    if @event_sessions.rows && @event_sessions.rows[0][-1] == "0" && !@event_sessions.rows[0][0].empty?
+      @warring[:event_sessions] = @event_sessions.rows[0]
+    end
+  end
+
+  def create_campaign_sessions
+    @campaign_sessions = @analytics.get_campaign_sessions(profile_id)
+  end
+
   def index
     @analytics = Analytics.new current_user
     profile_id = current_user.account_summary.default_profile
@@ -11,21 +22,15 @@ class DashboardController < ApplicationController
     # 測試1
     @act_users = @analytics.get_realtime_data(profile_id)
 
-    # 測試2
-    @event_sessions = @analytics.get_event_sessions(profile_id)
-    if @event_sessions.rows && @event_sessions.rows[0][-1] == "0" && !@event_sessions.rows[0][0].empty?
-      @warring[:event_sessions] = @event_sessions.rows[0]
-    end
-
-    # 測試3
-    @campaign_sessions = @analytics.get_campaign_sessions(profile_id)
-
+    create_event_sessions
+    create_campaign_sessions
   end
 
-  def authorize
-    redirect_to root_path unless user_signed_in?
-    if current_user.account_summary.default_profile.nil?
-      redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
+  private
+    def authorize
+      redirect_to root_path unless user_signed_in?
+      if current_user.account_summary.default_profile.nil?
+        redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
+      end
     end
-  end
 end
