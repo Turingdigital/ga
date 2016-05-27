e7430416d3037909c472443d0447c189ccc523b6
Isaac Tsai <isaac@adup.com.tw>
2016-05-27 18:29:03 +0800

    dashboard預警畫面收資料，待切版美化

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,35 +2,34 @@ class DashboardController < ApplicationController
 
   before_action :authorize, only: [:index]
 
-  def create_event_sessions
-    @event_sessions = @analytics.get_event_sessions(profile_id)
-    if @event_sessions.rows && @event_sessions.rows[0][-1] == "0" && !@event_sessions.rows[0][0].empty?
-      @warring[:event_sessions] = @event_sessions.rows[0]
-    end
-  end
-
-  def create_campaign_sessions
-    @campaign_sessions = @analytics.get_campaign_sessions(profile_id)
-  end
-
   def index
+    @warring = create_warrings
+  end
+
+  private
+
+  def create_warrings
     @analytics = Analytics.new current_user
     profile_id = current_user.account_summary.default_profile
-
-    @warring = {}
 
     # 測試1
     @act_users = @analytics.get_realtime_data(profile_id)
 
-    create_event_sessions
-    create_campaign_sessions
+    warring ={}
+    @event_sessions = @analytics.get_event_sessions(profile_id)
+    if @event_sessions.rows && @event_sessions.rows[0][-1] == "0" && !@event_sessions.rows[0][0].empty?
+      @warring[:event_sessions] = @event_sessions.rows[0]
+    end
+
+    @campaign_sessions = @analytics.get_campaign_sessions(profile_id)
+
+    return warring
   end
 
-  private
-    def authorize
-      redirect_to root_path unless user_signed_in?
-      if current_user.account_summary.default_profile.nil?
-        redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
-      end
+  def authorize
+    redirect_to root_path unless user_signed_in?
+    if current_user.account_summary.default_profile.nil?
+      redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
     end
+  end
 end
