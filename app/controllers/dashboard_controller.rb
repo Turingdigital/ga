57afb2fbcfc0fb3334b42fbaab5d02926d10cb59
Isaac Tsai <isaac@adup.com.tw>
2016-05-27 18:37:55 +0800

    重構主功能帳戶檢查過程 避免使用者為nil繼續執行

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,9 +27,12 @@ class DashboardController < ApplicationController
   end
 
   def authorize
-    redirect_to root_path unless user_signed_in?
-    if current_user.account_summary.default_profile.nil?
-      redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
+    if user_signed_in?
+      if current_user.account_summary.default_profile.nil?
+        redirect_to(account_summary_url(current_user.account_summary), flash: {alert: "你尚未設定預設設定檔"})
+      end
+    else
+      redirect_to root_path
     end
   end
 end
