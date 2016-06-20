4c5a577720d47513871348da6db961bc929ed922
Isaac Tsai <isaac@adup.com.tw>
2016-06-20 14:19:05 +0800

    使用者匯入CSV檔，媒介亂打直接當他使用'none'

--- app/models/url_builder.rb
+++ app/models/url_builder.rb
@@ -16,7 +16,8 @@ class UrlBuilder < ActiveRecord::Base
 
   def self.import file, user
     CSV.foreach(file.path, headers: true) do |row|
-      cm = CampaignMedium.where(medium: row[2]).first
+      cm = CampaignMedium.where(medium: row[2])
+      cm = cm.empty? CampaignMedium.where(medium: "none").first : cm.first
       UrlBuilder.create!(
         url: row[0],
         source: row[1],
