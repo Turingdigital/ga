b2cf3a3d6ef0157843e21f72bbbf1c96a28e5c99
Isaac Tsai <isaac@adup.com.tw>
2016-06-28 18:32:55 +0800

    AccountSummary解析出目前的GA設定檔各名稱

--- app/models/account_summary.rb
+++ app/models/account_summary.rb
@@ -5,6 +5,13 @@ class AccountSummary < ActiveRecord::Base
     return JSON.parse self.jsonString
   end
 
+  def default_ga_setting_names
+    h = {item: find_item}
+    h[:web_property] = find_web_property_from_item h[:item]
+    h[:profile] = find_profile_from_web_property h[:web_property]
+    return {item: h[:item]["name"], web_property: h[:web_property]["name"], profile: h[:profile]["name"]}
+  end
+
   class << self
 
     def re_fetch user
@@ -28,4 +35,17 @@ class AccountSummary < ActiveRecord::Base
       self.create(user: user, jsonString: result.to_json) if result
     end
   end
+
+  private
+    def find_item
+      return JSON.parse(self.jsonString)['items'].index_by{|item| item["id"]==self.default_item}[true]
+    end
+
+    def find_web_property_from_item item
+      return item["web_properties"].index_by{|web_property| web_property["id"]==self.default_web_property}[true]
+    end
+
+    def find_profile_from_web_property web_property
+      return web_property["profiles"].index_by{|profile| profile["id"]==self.default_profile}[true]
+    end
 end
