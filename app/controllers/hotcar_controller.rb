8295d76c1959c9ab30f3dcc4d33044d3fdda9ffc
Isaac Tsai <isaac@adup.com.tw>
2017-05-03 13:55:26 +0800

    remove byebug for production

--- app/controllers/hotcar_controller.rb
+++ app/controllers/hotcar_controller.rb
@@ -319,7 +319,6 @@ class HotcarController < ApplicationController
       "#{year_month_pre1_str}-01",
       "#{year_month_pre1_str}-#{'%02d' % previous_1_month_last_day}")
     event_pre1 = event_pre1["rows"] || {}
-    byebug
     event_pre1.map do |obj|
       obj[0] = obj[0].split(/:/).first
       obj[1] = obj[1].to_i
