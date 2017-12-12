d78c9c56084cedb7a8521feb0db61f24ef207365
Isaac Tsai <isaac@adup.com.tw>
2017-12-12 14:40:15 +0800

    myjapn 拿掉設定密碼輸入

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,10 @@ class WelcomeController < ApplicationController
     ana = Analytics.new u
     mj = ana.myjapanHK_auto('96435422')
     ary = mj["rows"]
-    ary.find_all {|ay| ay[0]=="確認點擊"}.map {|ay| ay[2]}.each{|_id| ary.delete_if {|ay| ay[2]==_id}}
+    ary.find_all {|ay|
+      ay[0]=="確認點擊"
+    }.map {|ay| ay[2]}.each{|_id|
+      ary.delete_if {|ay| ay[2]==_id||ay[0]=="設定密碼輸入"}}
     # ary.each {|ay| puts "#{ay[2]},會員註冊,#{ay[0]},#{ay[1]},#{ay[3]}"}
 
     ic = Iconv.new("big5", "utf-8")
