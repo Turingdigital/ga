5ecba3555e4851dd56fcf0576e39887289c49bc5
Isaac Tsai <isaac@adup.com.tw>
2017-12-15 10:00:09 +0800

    增加收件人email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
     attachments["#{filename}.csv"] = File.read(Rails.root+"public/csv/#{filename}.csv")
     mail bcc: ["isaac@turingdigital.com.tw"],
          to: ["pauline_huang@myday.com.tw"],
-         cc: ["jessie_chang@myday.com.tw", "bess@turingdigital.com.tw", "dennis@turingdigital.com.tw"],
+         cc: ["allen@myday.com.tw", "jessie_chang@myday.com.tw", "bess@turingdigital.com.tw", "dennis@turingdigital.com.tw"],
          subject: "圖靈05.My JAPAN HK 未註冊會員報表"
   end
 end
