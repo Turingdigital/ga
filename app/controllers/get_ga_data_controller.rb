20accaf0892ada95da2e15585b5672dce3d791f6
Isaac Tsai <isaac@adup.com.tw>
2018-01-23 11:35:12 +0800

    API可以送null值

--- app/controllers/get_ga_data_controller.rb
+++ app/controllers/get_ga_data_controller.rb
@@ -18,6 +18,25 @@ class GetGaDataController < ApplicationController
     # http://localhost:3000/get_ga_data
     # $.post( "http://localhost:3000/get_ga_data", {"profile_id":"154710634","start":"2017-12-30","end":"2018-01-05","metrics":["ga:sessions","ga:transactions","ga:transactionRevenue"],"dimensions":["ga:date","ga:hour","ga:userAgeBracket"],"filters":"ga:eventCategory==按鈕;ga:eventAction==點擊;ga:eventLabel=~點擊確認送出 - 進入付費頁面","segment":"sessions::condition::ga:deviceCategory==tablet,ga:deviceCategory==desktop","start_index":1}, function( data ) { console.log( data ); });
 
+# {
+#   "profile_id":"154710634",
+#   "start":"2017-12-30",
+#   "end":"2018-01-05",
+#   "metrics":[
+#     "ga:sessions",
+#     "ga:transactions",
+#     "ga:transactionRevenue"
+#   ],
+#   "dimensions":[
+#     "ga:date",
+#     "ga:hour",
+#     "ga:userAgeBracket"
+#   ],
+#   "filters":"ga:eventCategory==按鈕;ga:eventAction==點擊;ga:eventLabel=~點擊確認送出 - 進入付費頁面",
+#   "segment":"sessions::condition::ga:deviceCategory==tablet,ga:deviceCategory==desktop",
+#   "start_index":1
+# }
+
     u = User.where(email: "analytics@turingdigital.com.tw").first
     ana = Analytics.new(u)
     # ana.get_ga_data(
@@ -31,18 +50,19 @@ class GetGaDataController < ApplicationController
     #   segment,
     #   start_index,
     # )
+
     result = ana.get_ga_data(
       params[:profile_id],
       params[:start],
       params[:end],
       params[:metrics],
-      params[:dimensions],
+      params[:dimensions].empty? ? nil : params[:dimensions],
       nil, #params[:sort],
-      params[:filters],
-      params[:segment],
-      params[:start_index]
+      params[:filters].empty? ? nil : params[:filters],
+      params[:segment].empty? ? nil : params[:segment],
+      params[:start_index].empty? ? nil : params[:start_index]
     )
-    
+
     render json: result.to_json
   end
 end
