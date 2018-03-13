54bc4db6be4833dad4ad23e7f4c4eaadedb764a1
Isaac Tsai <isaac@adup.com.tw>
2018-03-13 14:46:05 +0800

    避免block 整個thread

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -17,15 +17,15 @@ class RobotsController < ApplicationController
 
       dl = URI::encode(target)
       begin
-        robot.count.times {
-          cid = "#{rand(10**9)}.#{rand(10**10)}"
-          url = "https://www.google-analytics.com/collect?v=1&tid=#{tid}&cid=#{cid}&t=pageview&dh=#{dh}&dp=#{dp}&dt=#{dt}&dl=#{dl}&ul=#{robot.ul}&geoid=#{robot.geoid}&sr=#{robot.sr}&vp=#{robot.vp}&ua=#{robot.ua}&sd=32-bits&fl=23.0 r0"
-          url = URI::encode(url)
-          Thread.new {
-            sleep(1+rand()*2)
+        Thread.new {
+          robot.count.times {
+            cid = "#{rand(10**9)}.#{rand(10**10)}"
+            url = "https://www.google-analytics.com/collect?v=1&tid=#{tid}&cid=#{cid}&t=pageview&dh=#{dh}&dp=#{dp}&dt=#{dt}&dl=#{dl}&ul=#{robot.ul}&geoid=#{robot.geoid}&sr=#{robot.sr}&vp=#{robot.vp}&ua=#{robot.ua}&sd=32-bits&fl=23.0 r0"
+            url = URI::encode(url)
+
+            sleep(2+rand()*4)
             open(url)
           }
-          sleep(1+rand()*2)
         }
       end
     }
